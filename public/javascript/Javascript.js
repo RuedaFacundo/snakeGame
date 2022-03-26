9dc38f516724359d6d04b6d4843dd58452b8685c
Facundo <facundoarueda@hotmail.com>
2022-03-26 16:30:38 -0300

    Animacion de botones joystick

--- public/javascript/Javascript.js
+++ public/javascript/Javascript.js
@@ -1,6 +1,6 @@
 let papel = document.querySelector("canvas");
 let context = papel.getContext("2d");
-var Modal = new bootstrap.Modal(document.getElementById('Modal'), {})
+var modal = new bootstrap.Modal(document.getElementById('Modal'), {})
 let botonModal = document.getElementById('botonModal');
 let superior = document.getElementById('superior');
 let fondo = document.getElementById('fondo');
@@ -12,14 +12,14 @@ let buttonClose = document.getElementById('buttonClose');
 let namePlayer = "";
 let scoreFinal = 0;
 
-/// Variables
-let adress; 
+// Variables
+let direction; 
 const INTERVALO = 80;
 const PESO = 10;
 const ANCHO = 500;
 let score = 1;
 
-/// Objetos
+// Objetos
 DIRECCIÓN = {      
 	A: [-1, 0], 
 	D: [1, 0],
@@ -35,19 +35,22 @@ DIRECCIÓN = {
     ArrowLeft: [-1, 0], 
 }
 
-/// Eventos para los botones de movimiento
-superior.addEventListener('click', function() {
-        superior.classList.add('active');
-        joystick(0, -1);
-        superior.classList.remove('active');
-    }) 
-fondo.addEventListener('click', function() { joystick(0, 1) }) 
-izquierda.addEventListener('click', function() { joystick(-1, 0) }) 
-derecha.addEventListener('click', function() { joystick(1, 0) })
+function Person(name, score) {
+    this.name = name;
+    this.score = score;
+}
+
+let arrayPersons = new Array();
+
+// Eventos para los botones de movimiento
+superior.addEventListener('click', function() {clickBtn(superior); joystick(0, -1); }) 
+fondo.addEventListener('click', function() {clickBtn(fondo); joystick(0, 1) }) 
+izquierda.addEventListener('click', function() {clickBtn(izquierda); joystick(-1, 0) }) 
+derecha.addEventListener('click', function() {clickBtn(derecha); joystick(1, 0) })
 
 function joystick (dx, dy) {
-    adress = [dx, dy]
-    const [x, y] = adress
+    direction = [dx, dy]
+    const [x, y] = direction
     if(-x !== controles.direccion.x && -y !== controles.direccion.y){
         controles.direccion.x = x;
         controles.direccion.y = y;
@@ -62,17 +65,49 @@ let controles = {
     crecimiento: 0
 }
 
-/// detectar cuando se apreta una tecla y hacer un condicional para no vuelva sobre si misma.
+// detectar cuando se apreta una tecla y hacer un condicional para no vuelva sobre si misma.
 document.onkeydown = (e) =>{
-    adress = DIRECCIÓN[e.key]
-    const [x, y] = adress
+    direction = DIRECCIÓN[e.key]
+    selectedKey(e.key);
+    const [x, y] = direction
     if(-x !== controles.direccion.x && -y !== controles.direccion.y){
         controles.direccion.x = x;
         controles.direccion.y = y;
     }
 }
 
-/// mueve la serpiente cada 80 milisegundos
+// Animacion al tocar una tecla
+function selectedKey (e) {
+    let key = e.toUpperCase();
+    switch (key) {
+        case "A":
+        case "ARROWLEFT":
+            clickBtn(izquierda);
+            break;
+        case "ARROWDOWN":
+        case "S":
+            clickBtn(fondo);
+            break;
+        case "ARROWUP":
+        case "W":
+            clickBtn(superior);
+            break;
+        case "ARROWRIGHT":
+        case "D":
+            clickBtn(derecha);
+            break;
+    }
+}
+
+// animacion de los botones del joystick
+function clickBtn (btn){
+    btn.classList.add("active")
+    setTimeout(function (){
+        btn.classList.remove("active")
+    }, 150);
+}
+
+// mueve la serpiente cada 80 milisegundos
 let looper = () => {
     let cola = {};
     Object.assign(cola, controles.bicho[controles.bicho.length -1])
@@ -85,7 +120,10 @@ let looper = () => {
         modalCierre.show();
         document.getElementById('nameModalFinal').innerHTML= "¡Congratulations! " + namePlayer;
         document.getElementById('scoreFinal').innerHTML= "Score: " + scoreFinal;
-        //reiniciar();
+        var person = new Person(namePlayer, scoreFinal);
+        arrayPersons.push(person);
+        updateTable(arrayPersons);
+        return;
     } 
     let dx = controles.direccion.x;
     let dy = controles.direccion.y;
@@ -116,7 +154,7 @@ let looper = () => {
     setTimeout(looper, INTERVALO);
 }
 
-/// metodo que dibuja la serpiente
+// metodo que dibuja la serpiente
 let dibujar = () => {
 	context.clearRect(0,0,ANCHO,ANCHO)
     for (let idx =0; idx < controles.bicho.length; idx ++){
@@ -132,7 +170,7 @@ let dibujarActores = (color, x, y) => {
     context.fillRect(x*PESO, y*PESO, PESO, PESO)
 }
 
-/// cuando come la victima, la reposiciona en otro lado.
+// cuando come la victima, la reposiciona en otro lado.
 let revictima = () => {
     let nuevaPosicion = random ();
     let victima = controles.victima;
@@ -142,11 +180,11 @@ let revictima = () => {
 
 let choque = () =>{
     const head = controles.bicho[0]
-    /// para verificar que no salga del canvas
+    // para verificar que no salga del canvas
     if(head.x <0 || head.x >=ANCHO/PESO || head.y<0 || head.y>=ANCHO/PESO ){
         return true;
     }
-    /// for para verificar que no choque sobre si misma
+    // for para verificar que no choque sobre si misma
     for(let idx = 1; idx < controles.bicho.length; idx ++){
         const sq = controles.bicho[idx];
         if (sq.x === head.x && sq.y === head.y){
@@ -156,7 +194,7 @@ let choque = () =>{
 
 }
 
-/// funcion que da posiciones random
+// funcion que da posiciones random
 let random = () => {
     let direc = Object.values(DIRECCIÓN);
     return {
@@ -166,17 +204,17 @@ let random = () => {
     }
 }
 
-/// funcion que reincia el juego en caso de choque 
+// funcion que reincia el juego en caso de choque 
 let reiniciar = () =>{
     controles = {direccion: {x:1, y:0}, bicho:[{x:0, y:0}], victima:{x:0, y:250}, jugando: false, crecimiento: 0}
-    /// random de la serpiente
+    // random de la serpiente
     posiciones = random();
     let head = controles.bicho[0]
     head.x = posiciones.x;
     head.y = posiciones.y;
     controles.direccion.x = posiciones.d[0];
     controles.direccion.y = posiciones.d[1];
-    /// random de la victima
+    // random de la victima
     posicionVictima = random();
     let victima = controles.victima;
     victima.x = posicionVictima.x;
@@ -186,23 +224,54 @@ let reiniciar = () =>{
 
 botonModal.addEventListener('click', function(){
     namePlayer = document.getElementById('name').value;
-    Modal.hide();
+    modal.hide();
     reiniciar();
     looper();
 })
 
 buttonClose.addEventListener('click', function(){
     modalCierre.hide();
-    //Modal.show();
+    modal.show()
 })
 
-/// cuando carga la web ejecuta looper
+// cuando carga la web ejecuta looper
 window.onload = () => {
-    Modal.show()
+    modal.show()
     modalCierre.hide();
-    ///reiniciar();
-    ///looper();
 }
 
-/// ideas
-/// con score a la hora de aumentar velocidad, si score es mayor a 5 duplicar velocidad etc.
+// ideas
+// con score a la hora de aumentar velocidad, si score es mayor a 5 duplicar velocidad etc.
+
+// para agregar a la tabla
+function updateTable (arrayPersons){
+    arrayPersons.sort(function (a, b) {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if (a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    });
+    deleteRow();
+
+    for (let i = 1; i <= arrayPersons.length && i < 6; i++) {
+        addrow(i, arrayPersons[i]);
+    }
+}
+
+function addrow(number, arrayPersons){
+    var table = document.getElementById("table");
+    var row = table.insertRow(0);
+    row.innerHTML = "<td>"+ number + "</td><td>" + arrayPersons.name + "</td><td>" + arrayPersons.score + "</td>";
+}
+
+function deleteRow(){
+    var table = document.getElementById("table");
+    var rowCount = table.rows.length;
+    
+    if(rowCount > 1){
+        table.deleteRow(rowCount -1);
+    }
+}
